da1b72432aeb686afaefc4291b7e9d217c151328
Peng Zhong <peng@nylira.com>
2015-01-22 01:54:04 +0800

    alrightgs

--- pub/js/app.js
+++ pub/js/app.js
@@ -1,15 +1,23 @@
-pixi = require('pixi')
+P = require('pixi.js')
 mousetrap = require('mousetrap')
 
+// globals
+var mapX = 512
+var mapY = 512
+
 // setup stage
-var stage = new pixi.Stage(0xCCD0CC)
-var renderer = pixi.autoDetectRenderer(480, 480)
+var stage = new P.Stage(0xCCD0CC)
+var renderer = P.autoDetectRenderer(mapX, mapY)
 document.body.appendChild(renderer.view);
 
 // setup textures
-var texture = pixi.Texture.fromImage('../img/block16x16.png')
-var cube = new pixi.Sprite(texture)
+var cubeTexture = P.Texture.fromImage('../img/block16x16.png')
+var cube = new P.Sprite(cubeTexture)
+var tileTexture = P.Texture.fromImage('../img/grid64x64.png')
+var tiles = new P.TilingSprite(tileTexture, mapX, mapX)
 
+// setup bg
+stage.addChild(tiles)
 
 // setup cube
 cube.position.x = 240
