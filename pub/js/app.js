270c45492a913a5c77a0b8e2d6396170665ff4cf
Peng Zhong <peng@nylira.com>
2015-01-22 13:27:32 +0800

    cleanup

--- pub/js/app.js
+++ pub/js/app.js
@@ -8,34 +8,39 @@ var combokeys = new Combokeys(document)
 var mapX = 512
 var mapY = 512
 var gu = 16
-var gameOver = false
+var gameOver
 
 // player variables
-var $direction = 'x'
-var delay = 100
+var $direction = null
+var refreshRate = 100//ms
 var alarm = new Date()
-alarm.setTime(new Date().getTime() + delay)
+alarm.setTime(new Date().getTime() + refreshRate)
 var cubes = []
 
+// stage variables
+var stage, renderer
+var tileTexture, cubeTexture, tiles, cube, cube1, cube2
 
+function preload() {
+  // setup stage
+  stage = new P.Stage(0xCCD0CC)
+  renderer = P.autoDetectRenderer(mapX, mapY)
+  document.body.appendChild(renderer.view);
 
-// setup stage
-var stage = new P.Stage(0xCCD0CC)
-var renderer = P.autoDetectRenderer(mapX, mapY)
-document.body.appendChild(renderer.view);
+  // setup textures
+  tileTexture = P.Texture.fromImage('../img/grid64x64.png')
+  cubeTexture = P.Texture.fromImage('../img/block16x16.png')
 
-// setup textures
-var tileTexture = P.Texture.fromImage('../img/grid64x64.png')
-var tiles = new P.TilingSprite(tileTexture, mapX, mapX)
-var cubeTexture = P.Texture.fromImage('../img/block16x16.png')
+  // setup sprites
+  tiles = new P.TilingSprite(tileTexture, mapX, mapX)
+  cube = new P.Sprite(cubeTexture)
+  cube1 = new P.Sprite(cubeTexture)
+  cube2 = new P.Sprite(cubeTexture)
+}
 
-var cube = new P.Sprite(cubeTexture)
-var cube1 = new P.Sprite(cubeTexture)
-var cube2 = new P.Sprite(cubeTexture)
-
-function initialize() {
+function init() {
   // keybindings
-  combokeys.bind(['up', 'w'], function() {$direction = 'n'; console.log("UP!!")})
+  combokeys.bind(['up', 'w'], function() {$direction = 'n'})
   combokeys.bind(['down','s'], function() {$direction = 's'})
   combokeys.bind(['left','a'], function() {$direction = 'e'})
   combokeys.bind(['right','d'], function() {$direction = 'w'})
@@ -57,26 +62,18 @@ function initialize() {
   cubes.push(cube1)
   cubes.push(cube2)
 }
-initialize()
 
-// animation loop
-if(gameOver !== true) {
-  requestAnimationFrame(animate);
-}
-
-function animate(){
+function update(){
   if(gameOver !== true) {
-    requestAnimationFrame(animate);
+    requestAnimationFrame(update);
   }
 
-  // move once every delay
+  // move once every refreshRate
   if(alarm.getTime() < new Date().getTime()) {
     move($direction)
-    alarm.setTime(new Date().getTime() + delay)
+    alarm.setTime(new Date().getTime() + refreshRate)
   }
-
   stayInBounds(cubes[0])
-
   renderer.render(stage)
 }
 
@@ -95,7 +92,7 @@ function move(dir) {
     case 'w':
       newCube = spawnCube(gu, 0)
       break
-    case 'x':
+    default:
       break
   }
 }
@@ -118,27 +115,41 @@ function spawnCube(offsetX, offsetY){
 }
 
 function stayInBounds(sprite) {
-  if(sprite.position.y < 0){
-    restartGame()
-  } else if (sprite.position.y > mapY) {
+  if(sprite.position.y < 0 || sprite.position.y > mapY) {
     restartGame()
   }
-  if (sprite.position.x < 0) {
-    restartGame()
-  } else if (sprite.position.x >= mapX) {
+  if (sprite.position.x < 0 || sprite.position.x >= mapX) {
     restartGame()
   }
 }
 
 function restartGame() {
+  // pause the game loop
   gameOver = true
-  console.log('game over. restarting...')
+  console.log('GAME OVER. Restarting...')
+
+  // unbind the hotkeys
   combokeys.reset()
+
+  // stop the snake from moving
   $direction = 'x'
+
+  // remove visual representation of snake
   for (var i=stage.children.length-1; i >= 0; i--) {
     stage.removeChild(stage.children[i])
   }
+
+  // remove elements from the snake
   cubes = []
-  initialize()
-  gameOver = false
+
+  startGame()
 }
+
+function startGame(){
+  gameOver = false
+  init()
+}
+
+preload()
+startGame()
+update()
